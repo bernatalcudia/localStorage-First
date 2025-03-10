ac969c625d08c06170946514fc271cd6c75f52ce
bernat alcudia <bernatalcudia@gmail.com>
2025-03-10 13:05:36 +0100

    Added values to localStorage

---
+++ sprint-05-02-js-localstorage-ejercicio/script.js
@@ -0,0 +1,11 @@
+// Crea las siguientes claves en el local storage: nombre, edad, profesion, CP, ciudad, teléfono y añade valores a cada uno de ellos
+// Accede a cada una de las claves sacando su valor y añadelas a un string que tenga sentido y muéstralas por consola. Por ejemplo "hola mi nombre completo es... tengo ... años..."
+// Modifica la ciudad por otra en la que te gustaría estar.
+// Elimina las claves CP y teléfono
+
+localStorage.setItem('nombre', 'Bernat');
+localStorage.setItem('edad', '25');
+localStorage.setItem('profesion', 'Desarrollador Full Stack Junior');
+localStorage.setItem('CP', '46600');
+localStorage.setItem('ciudad', 'Valencia');
+localStorage.setItem('Telefono', '1212323213');
